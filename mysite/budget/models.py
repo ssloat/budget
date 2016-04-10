9cc9852ef3e4022ad9c9cd20c474fdbe8047cafd
Stephen Sloat <stephenlsloat@gmail.com>
2016-04-09 23:50:50 -0500

    big changes

--- mysite/budget/models.py
+++ mysite/budget/models.py
@@ -13,13 +13,69 @@ class Budget(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     name = db.Column(db.String(64), nullable=False)
 
-    deductions = db.relationship('Deduction', backref='budget')
+    items = db.relationship('Item', backref='budget')
 
     def __init__(self, name):
         self.name = name
 
-class Deduction(db.Model):
-    __tablename__ = 'deductions'
+    def html(self):
+        results = {}
+        for item in self.items:
+            results[item.category] = results.get(item.category) or Table(item.category)
+            results[item.category].items.append(item)
+
+        results = sorted(results.values(), lambda x, y: cmp(x.sortkey, y.sortkey))
+        lines = sum([x.html for x in results], [])
+        lines.append("<tr><td colspan='6'>&nbsp</td></tr>")
+        lines.append(
+            "<tr><td colspan='4'>AGI</td><td class='num'>{:,.2f}</td></tr>".format(self.agi)
+        )
+
+        return lines
+        
+
+    @property
+    def income(self):
+        return sum([x.total for x in self.items if x.category == 'income'])
+
+    @property
+    def pretax(self):
+        return sum([x.total for x in self.items if x.category == 'pretax'])
+
+    @property
+    def agi(self):
+        return self.income - self.pretax
+
+class Table(object):
+    sortkeys = {'income': 1, 'pretax': 2}
+    def __init__(self, name):
+        self.name = name
+        self.sortkey = Table.sortkeys.get(name, 3)
+        self.items = []
+
+    @property
+    def total(self):
+        return sum([x.total for x in self.items])
+
+    def json(self):
+        return {
+            'name': self.name,
+            'items': [x.json() for x in self.items],
+            'total': '{:,.2f}'.format(sum([x.total for x in self.items])),
+        }
+
+    @property
+    def html(self):
+        lines = ["<tr><td colspan='6'>{}</td></tr>".format(self.name)]
+        for x in self.items:
+            lines.extend(x.html)
+        lines.append("<tr><td colspan='4'><td class='num'>{:,.2f}</td></tr>".format(self.total))
+
+        return lines
+
+
+class Item(db.Model):
+    __tablename__ = 'budget_items'
 
     id = db.Column(db.Integer, primary_key=True)
     budget_id = db.Column(db.Integer, db.ForeignKey('budgets.id'))
@@ -27,21 +83,50 @@ class Deduction(db.Model):
     name = db.Column(db.String(64), nullable=False)
     monthly = db.Column(db.Float, nullable=False)
     yearly = db.Column(db.Float, nullable=False)
-    pre_tax = db.Column(db.Boolean, nullable=False)
+    deductible = db.Column(db.Float, nullable=False)
 
-    def __init__(self, budget, category, name, monthly, yearly, pre_tax=None):
+    def __init__(self, budget, category, name, monthly, yearly, deductible=None):
         self.budget = budget
         self.category = category
         self.name = name
         self.monthly = monthly
         self.yearly = yearly
-        self.pre_tax = pre_tax or False
+        self.deductible = deductible or 0.0
 
+    @property
+    def total(self):
+        return 12*self.monthly + self.yearly
+
+    def json(self):
+        return {
+            'id': self.id,
+            'name': self.name,
+            'monthly': '{:,.2f}'.format(self.monthly),
+            'monthly12': '{:,.2f}'.format(12*self.monthly),
+            'yearly': '{:,.2f}'.format(self.yearly),
+            'total': '{:,.2f}'.format(self.total),
+        }
+
+    @property
     def html(self):
+        return (
+            "<tr dbid='{}' category='{}'>".format(self.id, self.category)
+            + "<td>{}</td>".format(self.name)
+            + "<td class='num'>{:,.2f}</td>".format(self.monthly)
+            + "<td class='num'>{:,.2f}</td>".format(12*self.monthly)
+            + "<td class='num'>{:,.2f}</td>".format(self.yearly)
+            + "<td class='num'>{:,.2f}</td>".format(self.total)
+            + "<td><input type='button' value='X'><input type='button' value='Edit'</td>"
+            + "</tr>",
+        )
+
+    def __html(self):
+        style = "style='width:100px; text-align:right'"
         return "<tr dbid=%d>" % self.id \
             + " <td style='width:200px' class='name'>%s</td>" % self.name \
-            + " <td style='width:100px; text-align:right' class='monthly'>%s</td>" % self.monthly \
-            + " <td style='width:100px; text-align:right'>%s</td>" % (12 * self.monthly) \
-            + " <td style='width:100px; text-align:right' class='monthly'>%s</td>" % self.yearly \
-            + " <td style='width:100px; text-align:right' class='amount'>%s</td>" % (12*self.monthly + self.yearly) \
+            + " <td {0} class='monthly'>{1:,.2f}</td>".format(style, self.monthly) \
+            + " <td {0}>{1:,.2f}</td>".format(style, 12 * self.monthly) \
+            + " <td {0} class='yearly'>{1:,.2f}</td>".format(style, self.yearly) \
+            + " <td {0} class='amount'>{1:,.2f}</td>".format(style, 12*self.monthly + self.yearly) \
+            + " <td > <input type='button' value='X'> </td>" \
             + " </tr>"
--- mysite/budget/views.py
+++ mysite/budget/views.py
@@ -1,20 +1,20 @@
 import collections
 
 from mysite import app, db
-from mysite.budget.models import Budget, Deduction
+from mysite.budget.models import Budget, Item
 
 from flask import jsonify, render_template, request
 
+@app.template_filter('money')
+def money_filter(s):
+    return "{:,.2f}".format(s)
 
 @app.route('/rest/budget/<int:budget_id>', methods=['GET'])
 def rest_budget(budget_id):
     b = db.session.query(Budget).filter(Budget.id==budget_id).first()
 
-    results = collections.defaultdict(list)
-    for d in b.deductions:
-        results[d.category].append(d.html())
-
-    return jsonify({ 'budget': results })
+    data = {'name': b.name, 'rows': b.html() }
+    return jsonify(data)
 
 @app.route('/rest/add_item/<int:budget_id>', methods=['POST'])
 def rest_add_item(budget_id):
@@ -22,8 +22,11 @@ def rest_add_item(budget_id):
 
     json = request.form
 
+    monthly = float(json['monthly']) / 12.0 if json['monthly'] else 0.0
+    yearly = float(json['yearly']) if json['yearly'] else 0.0
+
     db.session.add( 
-        Deduction(b, json['category'], json['name'], json['monthly'], json['yearly']) 
+        Item(b, json['category'], json['name'], monthly, yearly) 
     )
     db.session.commit()
 
@@ -35,7 +38,7 @@ def rest_remove_item(budget_id):
 
     json = request.form
 
-    db.session.query(Deduction).filter(Deduction.id==json['dbid']).delete()
+    db.session.query(Item).filter(Item.id==json['dbid']).delete()
     db.session.commit()
 
     return rest_budget(budget_id)
